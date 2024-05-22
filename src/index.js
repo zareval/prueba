64feb94d58bb0614e05d3fbb8b0dee019923e7fd
Zare <zarellaburbanov@gmail.com>
2024-05-22 16:02:46 -0500

    ajuste

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
   // src/index.js
   import React from 'react';
   import ReactDOM from 'react-dom';
- import './index.css';
+  import './index.css';
   import App from './App';
-  import reportWebVitals from './reportWebVitals';
 
   ReactDOM.render(
     <React.StrictMode>
