cd2b736cdcd9ae13b21300498086f050abc40d44
Zare <zarellaburbanov@gmail.com>
2024-05-25 11:21:10 -0500

    front

--- src/HomePage.js
+++ src/HomePage.js
@@ -21,7 +21,6 @@ function HomePage() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold mb-6 text-center">Lista de Películas</h1>
       {movies.length > 0 ? (
         <ul className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {movies.map((movie) => (
